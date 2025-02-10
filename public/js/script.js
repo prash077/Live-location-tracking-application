b52cbe93b26aff749f6c4357154bc3593feba754
unknown <rprashasthimmanuel@gmail.com>
2025-02-11 00:09:11 +0530

    LAAYERS - MARKERS - LATLON Established successfully....!

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,8 @@ if(navigator.geolocation)
 {
     navigator.geolocation.watchPosition(
         (position)=>{
-            const { latitude, longtitude } = position.coords;
+            const { latitude, longitude } = position.coords;
+            socket.emit("send-location", { latitude, longitude });
         },
         (error)=>{
             console.log(error);
@@ -22,3 +23,17 @@ const map = L.map("map").setView([0,0],10);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
     attribution:"@Track-live-location"
 }).addTo(map);
+
+const marker = {};
+
+socket.on("receive-location", (data)=>{
+    const {id, latitude, longitude} = data;
+    map.setView([latitude,longitude],16);
+    if(marker[id])
+    {
+        marker[id].setLatLng([latitude,longitude]);
+    }
+    else{
+        marker[id] = L.marker([latitude,longitude]).addTo(map);
+    }
+});
